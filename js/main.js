d2681ad5051376ce741d1667a5c8c9d00b1a89a6
Bojan Popov <bp2307994450145@gmail.com>
2020-11-08 21:12:20 -1200

    Homework final

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(function () {
+    const buttons = document.querySelectorAll(".btn");
+    const storeItems = document.querySelectorAll(".store-item");
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        e.preventDefault();
+        const filter = e.target.dataset.filter;
+  
+        storeItems.forEach((item) => {
+          if (filter === "all") {
+            item.style.display = "block";
+          } else {
+            if (item.classList.contains(filter)) {
+              item.style.display = "block";
+            } else {
+              item.style.display = "none";
+            }
+          }
+        });
+      });
+    });
+  })();
+  
+  (function () {
+    const searchBox = document.querySelector("#search-item");
+    const storeItems = document.querySelectorAll(".store-item");
+  
+    searchBox.addEventListener("keyup", (e) => {
+      const searchFilter = e.target.value.toLowerCase().trim();
+  
+      storeItems.forEach((item) => {
+        if (
+          item.querySelector("h5") &&
+          item
+            .querySelector("h5")
+            .textContent.toLowerCase()
+            .includes(searchFilter)
+        ) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  })();
+  
